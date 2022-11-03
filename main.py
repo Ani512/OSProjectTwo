ca4b151443212357ff4aacdeed6b05756c402594
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-11-02 22:22:44 -0500

    05

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ maxCustomers = 50
 totalTellers = [False, False, False]
 
 # ONLY TWO Tellers are allowed inside the safe
-safeSemaphore = Semaphore(2) 
+safeSemaphore = Semaphore(2)
 
 # ONLY 1 MANAGER
 managerSemaphore = Semaphore(1)
@@ -18,12 +18,14 @@ tellerSemaphore = Semaphore(3)
 
 printLock = Lock()
 
+
 class Teller:
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
         return f"{self.name}"
+
 
 class Customer:
     def __init__(self, name):
@@ -49,25 +51,26 @@ def printUsingLock(msg):
 
 
 def customerRun(customer, tellerQueue):
-    depositOrWithdraw = random.choice([0, 1]) # gives type of transaction
+    depositOrWithdraw = random.choice([0, 1])  # gives type of transaction
     transactionType = ''
-    
+
     if depositOrWithdraw == 0:
         transactionType = 'deposit'
         printUsingLock(f"{customer} wants to DEPOSIT")
-    
+
     else:
         transactionType = 'withdraw'
         printUsingLock(f"{customer} wants to WITHDRAW")
     customer.setTransactionType(transactionType)
-    time.sleep(random.uniform(0, 2.5)); 
+    # customer takes some time to go to the bank
+    time.sleep(random.uniform(0.05, 0.1))
     printUsingLock(f"{customer} is going to the bank")
     try:
         printUsingLock(f"{customer} is getting in line")
         try:
             tellerSemaphore.acquire()
         except:
-            print("Error while acquiring teller thread") 
+            print("Error while acquiring teller thread")
         printUsingLock(f"{customer} is selecting a teller")
 
 # the False check lets me know if a teller is busy or not
@@ -89,8 +92,8 @@ def customerRun(customer, tellerQueue):
 def tellerRun(teller, tellerQueue):
     currentTeller = teller.name
     printUsingLock(f"{currentTeller} is ready to serve")
-    
-    currentTellerNumber = int(currentTeller[7]) 
+
+    currentTellerNumber = int(currentTeller[7])
     while True:
         try:
             printUsingLock(f"{currentTeller} is waiting for a customer")
@@ -98,32 +101,37 @@ def tellerRun(teller, tellerQueue):
             customer = tellerQueue.get(timeout=2)
             printUsingLock(f"{customer} introduces itself to {currentTeller}")
             printUsingLock(f"{currentTeller} is serving {customer}")
-            printUsingLock(f"{customer} asks for a {customer.getTransactionType()} transaction")
-            printUsingLock(f"{currentTeller} is handling the {customer.getTransactionType()} transaction")
-            
+            printUsingLock(
+                f"{customer} asks for a {customer.getTransactionType()} transaction")
+            printUsingLock(
+                f"{currentTeller} is handling the {customer.getTransactionType()} transaction")
+
             if customer.transactionType == 'withdraw':
                 printUsingLock(f"{currentTeller} is going to the manager")
                 try:
                     managerSemaphore.acquire()
                 except:
-                    print("Error while going to Manager")  
-                printUsingLock(f"{currentTeller} is getting the manager's permission")
+                    print("Error while going to Manager")
+                printUsingLock(
+                    f"{currentTeller} is getting the manager's permission")
                 # the teller thread should block for a random duration from 5 to 30 ms
-                time.sleep(random.uniform(0.05, 0.3))
-                printUsingLock(f"{currentTeller} has got the manager's permission")
+                time.sleep(random.uniform(0.005, 0.03))
+                printUsingLock(
+                    f"{currentTeller} has got the manager's permission")
                 managerSemaphore.release()
-            
+
             printUsingLock(f"{currentTeller} is going to the safe")
             try:
                 safeSemaphore.acquire()
             except:
-                print("Error while going to safe")    
+                print("Error while going to safe")
             printUsingLock(f"{currentTeller} is in the safe")
             # the teller will physically perform the transaction by blocking for a random duration of between 10 and 50 ms
-            time.sleep(random.uniform(0.1, 0.5))
+            time.sleep(random.uniform(0.01, 0.05))
             printUsingLock(f"{currentTeller} is leaving the safe")
             safeSemaphore.release()
-            printUsingLock(f"{currentTeller} finishes {customer}'s {customer.getTransactionType()} transaction")
+            printUsingLock(
+                f"{currentTeller} finishes {customer}'s {customer.getTransactionType()} transaction")
             printUsingLock(f"{customer} thanks {currentTeller} and leaves")
             totalTellers[currentTellerNumber-1] = False
             tellerSemaphore.release()
@@ -144,8 +152,9 @@ if __name__ == '__main__':
 # creating threads for the tellers
     tellerThreadList = []
     for teller in range(0, len(tellers)):
-        tellerThreadList.append(Thread(name="tellerThread"+str(teller), target=tellerRun, args=(tellers[teller], tellerQueue)))
-    
+        tellerThreadList.append(Thread(
+            name="tellerThread"+str(teller), target=tellerRun, args=(tellers[teller], tellerQueue)))
+
 # starting the threads
     for telThread in tellerThreadList:
         telThread.start()
@@ -154,11 +163,12 @@ if __name__ == '__main__':
     customers = []
     for customer in range(1, maxCustomers + 1):
         customers.append(Customer("Customer " + str(customer)))
-       
-# creating threads for the 50 customers 
+
+# creating threads for the 50 customers
     customerThreadList = []
     for customer in range(0, len(customers)):
-        customerThreadList.append(Thread(name="customerThread"+str(customer), target=customerRun, args=(customers[customer], tellerQueue)))
+        customerThreadList.append(Thread(name="customerThread"+str(customer),
+                                  target=customerRun, args=(customers[customer], tellerQueue)))
 
 # starting the customer threads
     for custThread in customerThreadList:
